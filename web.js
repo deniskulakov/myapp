dedb6ac1c69684fe35fc7e5beeba273bbea114bf
deniskulakov <deniskulakov@MacBook-Air-Denis.local>
2013-10-26 22:23:34 +0200

    first commit of all

---
+++ web.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+var express = require('express');
+var app = express();
+app.use(express.logger());
+
+app.get('/', function(request, response) {
+  response.send('Hello World! This is DEV env');
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
